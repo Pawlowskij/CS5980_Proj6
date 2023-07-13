cf33caeae97fd5b4b76f4328db8074ba75e803ee
TylerGraham4 <grahamt@msoe.edu>
2023-07-13 18:35:01 -0500

    Initial committ for bulette test files

--- Proj6/test_bulette.py
+++ Proj6/test_bulette.py
@@ -1,56 +1,56 @@
 import pytest
 
-from mugwump import RealMugwump
+from bulette import Bulette
 from die import Die
 
-# create fixture mugwump
+# create fixture bulette
 @pytest.fixture
-def my_mugwump():
-    mugwump = RealMugwump(1)
+def my_bulette():
+    bulette = Bulette(1)
     d10 = Die(10)
 
     # artificially set max hitpoints
-    mugwump.maxHitPoints = 20
-    mugwump.hitPoints = 20
-    return mugwump
+    bulette.maxHitPoints = 60
+    bulette.hitPoints = 60
+    return bulette
 
 # test that mugwump HP is same as the Max HP at initialization
 def test_Mugwump_hitpoints():
     # Create a Mugwump object
-    mugwumpHP = RealMugwump(1)
-    assert mugwumpHP.maxHitPoints == mugwumpHP.hitPoints
+    buletteHP = Bulette(1)
+    assert buletteHP.maxHitPoints == buletteHP.hitPoints
     # Test that HP is between 6 and 60
-    assert mugwumpHP.maxHitPoints >= 6 and mugwumpHP.maxHitPoints <= 60
+    assert 6 <= buletteHP.maxHitPoints <= 60
 
 
-def test_take_damage(my_mugwump):
+def test_take_damage(my_bulette):
     # check for damage of 5 resulting in 15
-    my_mugwump.takeDamage(5)
-    assert (my_mugwump.hitPoints == 15)
+    my_bulette.takeDamage(5)
+    assert (my_bulette.hitPoints == 55)
 
     # overheal should return max HP
-    my_mugwump.takeDamage(-100)
-    assert (my_mugwump.hitPoints == 20)
+    my_bulette.takeDamage(-100)
+    assert (my_bulette.hitPoints == 60)
 
     # overkill should return 0 HP
-    my_mugwump.takeDamage(2000000)
-    assert (my_mugwump.hitPoints == 0)
+    my_bulette.takeDamage(2000000)
+    assert (my_bulette.hitPoints == 0)
 
     # Test valid attack choice
-def test_attackChoice(monkeypatch, my_mugwump):
+def test_attackChoice(monkeypatch, my_bulette):
     # Simulate user input '1' for claws attack
     monkeypatch.setattr('builtins.input', lambda _: '1')
-    choice = my_mugwump.attackChoice()
+    choice = my_bulette.attackChoice()
     assert choice == 1
 
     # Simulate user input '2' for fangs attack
     monkeypatch.setattr('builtins.input', lambda _: '2')
-    choice = my_mugwump.attackChoice()
+    choice = my_bulette.attackChoice()
     assert choice == 2
 
     # Simulate user input '3' for heals attack
     monkeypatch.setattr('builtins.input', lambda _: '3')
-    choice = my_mugwump.attackChoice()
+    choice = my_bulette.attackChoice()
     assert choice == 3
 
     # # Test invalid attack choice -- creates an infinite loop because of While True in attackChoice??
@@ -61,51 +61,51 @@ def test_attackChoice(monkeypatch, my_mugwump):
 
 
 
-def test_attack(my_mugwump):
+def test_attack(my_bulette):
     # # test attack with claws
-    my_mugwump.attackChoice = lambda: 1
-    damage = my_mugwump.attack()
+    my_bulette.attackChoice = lambda: 1
+    damage = my_bulette.attack()
     assert damage >= 0
-    assert my_mugwump.hitPoints == 20  # Hit points should remain unchanged
+    assert my_bulette.hitPoints == 60  # Hit points should remain unchanged
 
     # test attack with fangs
-    my_mugwump.attackChoice = lambda: 2
-    damage = my_mugwump.attack()
+    my_bulette.attackChoice = lambda: 2
+    damage = my_bulette.attack()
     assert damage >= 0
-    assert my_mugwump.hitPoints == 20  # Hit points should remain unchanged
+    assert my_bulette.hitPoints == 60  # Hit points should remain unchanged
 
     # test heal
-    injuredMugwump = RealMugwump(1)
+    injuredBulette = Bulette(1)
     # artificially set max hitpoints
-    injuredMugwump.maxHitPoints = 35
+    injuredBulette.maxHitPoints = 55
     #injury Mugwump to allow for heal
-    injuredMugwump.hitPoints = 20
-    my_mugwump.attackChoice = lambda: 3
-    damage = my_mugwump.attack()
+    injuredBulette.hitPoints = 60
+    my_bulette.attackChoice = lambda: 3
+    damage = my_bulette.attack()
     assert damage < 0
     # apply heal
-    injuredMugwump.takeDamage(damage)
-    assert injuredMugwump.hitPoints > 20  # Hit points should remain unchanged
+    injuredBulette.takeDamage(damage)
+    assert injuredBulette.hitPoints > 20  # Hit points should remain unchanged
 
-def test_ai(my_mugwump):
+def test_ai(my_bulette):
     # Test attack_type 1 (Razor-Sharp Claws)
     # Make the d20 roll to return 12 (<=12)
-    my_mugwump.d20.roll = lambda: 12
-    assert my_mugwump._RealMugwump__ai() == 1
+    my_bulette.d20.roll = lambda: 12
+    assert my_bulette._Bulette__ai() == 1
 
     # Test attack_type 2 (Their Fangs of Death)
     # Make the d20 roll to return 17 (12 < x <= 17)
-    my_mugwump.d20.roll = lambda: 17
-    assert my_mugwump._RealMugwump__ai() == 2
+    my_bulette.d20.roll = lambda: 17
+    assert my_bulette._Bulette__ai() == 2
 
     # Test attack_type 3 (Heal)
     # Make the d20 roll to return 18 (>17)
-    my_mugwump.d20.roll = lambda: 18
-    assert my_mugwump._RealMugwump__ai() == 3
+    my_bulette.d20.roll = lambda: 18
+    assert my_bulette._Bulette__ai() == 3
 
     # Test attack_type 2 when d20 roll is 13
     # Make the d20 roll to return 13 (equal to 13)
-    my_mugwump.d20.roll = lambda: 13
-    assert my_mugwump._RealMugwump__ai() == 2
+    my_bulette.d20.roll = lambda: 13
+    assert my_bulette._Bulette__ai() == 2
 
 
